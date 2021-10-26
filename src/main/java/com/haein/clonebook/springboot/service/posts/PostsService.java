b06b7294400d5a2e4f9d96edd4927480ddce3f61
HAEIN <grep.haein@gmail.com>
2021-10-26 22:46:10 +0900

    3.4 완료

---
+++ src/main/java/com/haein/clonebook/springboot/service/posts/PostsService.java
@@ -0,0 +1,40 @@
+package com.haein.clonebook.springboot.service.posts;
+
+import com.haein.clonebook.springboot.domain.posts.Posts;
+import com.haein.clonebook.springboot.domain.posts.PostsRepository;
+import com.haein.clonebook.springboot.web.dto.PostsResponseDto;
+import com.haein.clonebook.springboot.web.dto.PostsSaveRequestDto;
+import com.haein.clonebook.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+
+}
---
+++ src/main/java/com/haein/clonebook/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,14 @@
+package com.haein.clonebook.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
+
